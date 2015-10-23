dbb66b28560318e5143bb02631515f716c393273
Guillermo Alvarez <galvarezcity@me.com>
2015-10-23 13:48:44 -0700

    starter project file added

---
+++ Q/Q/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Q
+//
+//  Created by Archie on 10/23/15.
+//  Copyright © 2015 SquirrelApps. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var classText: UITextField!
+    
+    @IBAction func send(sender: UIButton) {
+        sendInfo()
+        view.endEditing(true)
+        name.text = ""
+        classText.text = ""
+    }
+    
+    
+    func sendInfo(){
+        if let n = name.text, c = classText.text{
+        let person = PFObject(className: "Person")
+            person["name"] = n
+            person["class"] = c
+        person.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

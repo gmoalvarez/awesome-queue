61d3f5f6cbc321b1d0ec94c9a60cc0ee1a7df760
Guillermo Alvarez <galvarezcity@me.com>
2015-11-04 13:00:21 -0800

    Recreated files again (were incorrect type first time)

--- Q/Q/QueueListCell.swift
+++ Q/Q/QueueListCell.swift
@@ -6,8 +6,19 @@
 //  Copyright © 2015 SquirrelApps. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class QueueListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 
 }

92da4896b502ebceace3413dab332e3a52aec896
Guillermo Alvarez <galvarezcity@me.com>
2015-11-03 19:43:36 -0800

    Added outlets and IBActions for login/signup page

---
+++ Q/Q/LoginSignupViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoginSignupViewController.swift
+//  FInalProject
+//
+//  Created by Guillermo on 11/3/15.
+//  Copyright © 2015 SquirrelApps. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignupViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func signupPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginPressed(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
